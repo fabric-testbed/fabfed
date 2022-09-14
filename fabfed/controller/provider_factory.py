efad7fda0d49baa1e7c5aa827f29d35f0d8258f3
Abdelilah Essiari <aessiari@lbl.gov>
2022-09-13 21:48:55 -0700

    enhanced error handling in chi provider.added stitich unit test.  moved dependency handling to base/model classes. added friendly name for tool

--- fabfed/controller/provider_factory.py
+++ fabfed/controller/provider_factory.py
@@ -6,27 +6,27 @@ class ProviderFactory:
     def __init__(self):
         self._providers: Dict[str, Provider] = {}
 
-    def init_provider(self, kind: str, name: str, attributes, logger):
-        if kind == 'fabric':
+    def init_provider(self, *, type: str, label: str, attributes, logger):
+        if type == 'fabric':
             from fabfed.provider.fabric.fabric_provider import FabricProvider
 
-            provider = FabricProvider(type=kind, name=name, logger=logger, config=attributes)
+            provider = FabricProvider(type=type, name=label, logger=logger, config=attributes)
             provider.setup_environment()
-            self._providers[name + '@' + kind] = provider
-        elif kind == 'chi':
+            self._providers[label] = provider
+        elif type == 'chi':
             from fabfed.provider.chi.chi_provider import ChiProvider
 
-            provider = ChiProvider(type=kind, name=name, logger=logger, config=attributes)
-            self._providers[name + '@' + kind] = provider
+            provider = ChiProvider(type=type, name=label, logger=logger, config=attributes)
+            self._providers[label] = provider
         else:
-            raise Exception(f"no provider for type {kind}")
+            raise Exception(f"no provider for type {type}")
 
     @property
     def providers(self) -> List[Provider]:
         return list(self._providers.values())
 
-    def get_provider(self, kind: str, name: str) -> Provider:
-        return self._providers[name + '@' + kind]
+    def get_provider(self, *, label: str) -> Provider:
+        return self._providers[label]
 
 
-provider_factory = ProviderFactory()
+default_provider_factory = ProviderFactory()
--- fabfed/provider/fabric/fabric_network.py
+++ fabfed/provider/fabric/fabric_network.py
@@ -28,22 +28,30 @@ from ipaddress import IPv4Address, IPv4Network
 from fabrictestbed_extensions.fablib.network_service import NetworkService
 from fabrictestbed_extensions.fablib.slice import Slice
 
-from fabfed.util.config import Config
 from fabfed.model import Network
+from fabfed.util.config import Config
 
 
 class FabricNetwork(Network):
-    def __init__(self, *, delegate: NetworkService, subnet: IPv4Network, pool_start: IPv4Address, pool_end: IPv4Address):
-        self._delegate = delegate
-        self._subnet = subnet
-        self._pool_start = pool_start
-        self._pool_end = pool_end
+    def __init__(self, *, label, delegate: NetworkService, subnet: IPv4Network, pool_start: IPv4Address,
+                 pool_end: IPv4Address):
         self.name = delegate.get_name()
+        self.site = delegate.get_site()
+        super().__init__(label=label, name=self.name, site=self.site)
+        self._delegate = delegate
+        self.site = delegate.get_site()
+        self.slice_name = self._delegate.get_slice().get_name()
+        self.type = str(delegate.get_type())
+        self.subnet = str(subnet)
+        self.pool_start = str(pool_start)
+        self.pool_end = str(pool_end)
 
     def available_ips(self):
         available_ips = []
+        pool_start = int(IPv4Address(self.pool_start))
+        pool_end = int(IPv4Address(self.pool_end))
 
-        for ip_int in range(int(self._pool_start) + 1, int(self._pool_end) + 1):
+        for ip_int in range(pool_start + 1, pool_end + 1):
             available_ips.append(IPv4Address(ip_int))
 
         return available_ips
@@ -56,13 +64,15 @@ class FabricNetwork(Network):
 
 
 class NetworkBuilder:
-    def __init__(self,  slice_object: Slice, resource: dict):
+    def __init__(self, label, slice_object: Slice, resource: dict):
         self.slice_object = slice_object
         self.facility_port_vlan = None
 
         for resolved_dependency in resource['resolved_dependencies']:
             self.facility_port_vlan = str(resolved_dependency.value[0])
             break
+
+        assert self.facility_port_vlan, "missing vlan"
 
         self.facility_port = 'Chameleon-StarLight'
         self.facility_port_site = resource.get(Config.RES_SITE)
@@ -73,6 +83,7 @@ class NetworkBuilder:
         self.subnet = IPv4Network(resource.get(Config.RES_SUBNET))
         self.pool_start = IPv4Address(resource.get(Config.RES_NET_POOL_START))
         self.pool_end = IPv4Address(resource.get(Config.RES_NET_POOL_END))
+        self.label = label
         self.net = None
 
     def handle_facility_port(self):
@@ -84,6 +95,7 @@ class NetworkBuilder:
     def handle_l2network(self, interfaces):
         assert len(interfaces) > 0
         assert len(self.interfaces) == 1
+
         self.interfaces.extend(interfaces)
         self.net: NetworkService = self.slice_object.add_l2network(name=self.net_name, interfaces=self.interfaces)
 
@@ -92,4 +104,5 @@ class NetworkBuilder:
         assert self.subnet
         assert self.pool_start
         assert self.pool_end
-        return FabricNetwork(delegate=self.net, subnet=self.subnet, pool_start=self.pool_start, pool_end=self.pool_end)
+        return FabricNetwork(label=self.label, delegate=self.net, subnet=self.subnet, pool_start=self.pool_start,
+                             pool_end=self.pool_end)
