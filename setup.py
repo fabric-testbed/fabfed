694e387ab4ea759f562120ce25b36e79878ab4de
Abdelilah Essiari <aessiari@lbl.gov>
2022-09-27 09:50:30 -0700

    workaround to install dependencies and fabfed-py

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.9',
     entry_points={
        'console_scripts': [
               'fabfed = tools.fabfed:main',
