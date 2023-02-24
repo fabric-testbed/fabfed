6d4fdf63b7b1f8fdbd6c669dedacca723e3efc66
Abdelilah Essiari <aessiari@lbl.gov>
2023-02-23 19:54:40 -0800

    initial cloudlab. tested with fabfed-stitch

---
+++ fabfed/provider/cloudlab/cloudlab_exceptions.py
@@ -0,0 +1,16 @@
+from fabfed.exceptions import FabfedException
+
+
+class CloudlabException(FabfedException):
+    """Cloudlab exception"""
+    def __init__(self, *, message=None, exitval, response=None):
+        self.message = f"exitval={exitval}"
+
+        if message:
+            self.message = f"message={message},{self.message}"
+
+        if response:
+            self.message = f"{self.message},code={response.code},output={response.output}"
+
+        self.message = f"[{self.message}]"
+        super().__init__(self.message)
