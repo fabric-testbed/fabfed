052163f90ca72269bfccd99e3e5e3a78fb89ed68
Abdelilah Essiari <aessiari@lbl.gov>
2023-05-17 09:46:11 -0700

    fixed loading of sense config

--- fabfed/provider/sense/sense_client.py
+++ fabfed/provider/sense/sense_client.py
@@ -13,13 +13,14 @@ def init_client(config):
 # TODO Fix this and make sure the config is coming from fabfed credentials
 class SenseClient(RequestWrapper):
     def __init__(self, config=None):
-        self.config = config
+        self.temp_config = config
         super().__init__()
 
     def getConfig(self, config_file=None):
-        if not self.config:
+        if not self.temp_config:
             super().getConfig()
             return
 
+        self.config = self.temp_config
         self._validateConfig()
         self._setDefaults()
