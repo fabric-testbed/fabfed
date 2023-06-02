4f40ec7cd157605347ea83867452205437486097
Liang Zhang <lzhang9@es.net>
2023-06-02 17:56:37 -0500

    Update sense_constants.py

--- fabfed/provider/sense/sense_constants.py
+++ fabfed/provider/sense/sense_constants.py
@@ -15,8 +15,10 @@ SERVICE_INSTANCE_KEYS = ['intents', 'alias', 'referenceUUID', 'state', 'owner', 
 
 SENSE_CUSTOMER_ASN = "customer_asn"
 SENSE_AMAZON_ASN = "amazon_asn"
+SENSE_GOOGLE_ASN = "google_asn"
 SENSE_CUSTOMER_IP = "customer_ip"
 SENSE_AMAZON_IP = "amazon_ip"
+SENSE_GOOGLE_IP = "google_ip"
 SENSE_AUTHKEY = "authkey"
 SENSE_TO_HOSTED_CONN = "to_hosted_conn"
 
@@ -26,6 +28,18 @@ SENSE_AWS_PEERING_MAPPING = {
 
     Constants.RES_REMOTE_ASN: SENSE_AMAZON_ASN,
     Constants.RES_REMOTE_ADDRESS: SENSE_AMAZON_IP,
+
+    Constants.RES_SECURITY: SENSE_AUTHKEY,
+
+    Constants.RES_ID: SENSE_TO_HOSTED_CONN,
+}
+
+SENSE_GCP_PEERING_MAPPING = {
+    Constants.RES_LOCAL_ASN: SENSE_CUSTOMER_ASN,
+    Constants.RES_LOCAL_ADDRESS: SENSE_CUSTOMER_IP,
+
+    Constants.RES_REMOTE_ASN: SENSE_GOOGLE_ASN,
+    Constants.RES_REMOTE_ADDRESS: SENSE_GOOGLE_IP,
 
     Constants.RES_SECURITY: SENSE_AUTHKEY,
 
