16f67b7726c8bc08591a81480b9bba74314e6b74
Abdelilah Essiari <aessiari@lbl.gov>
2023-08-16 08:58:47 -0700

    aws:creating vpn gateway and vif ...

--- fabfed/provider/aws/aws_constants.py
+++ fabfed/provider/aws/aws_constants.py
@@ -1,4 +1,12 @@
-ACCESS_KEY="ACCESS_KEY"
-SECRET_KEY="SECRET_KEY"
+ACCESS_KEY = "ACCESS_KEY"
+SECRET_KEY = "SECRET_KEY"
 RETRY = 60
-BGPKEY="0xzsEwC7xk6c1fK_h.xHyAdx"
+BGPKEY = "0xzsEwC7xk6c1fK_h.xHyAdx"
+
+VLAN = 'vlan'
+AMAZON_SIDE_ASN = 'amazonSideAsn'
+ASN = 'asn'
+VIF_ID = 'virtualInterfaceId'
+VIF_STATE = 'virtualInterfaceState'
+
+VIF_DETAILS = [VLAN, AMAZON_SIDE_ASN, ASN, VIF_ID, VIF_STATE]
--- fabfed/provider/aws/aws_utils.py
+++ fabfed/provider/aws/aws_utils.py
@@ -20,18 +20,120 @@ def create_ec2_client(*, region: str, access_key: str, secret_key: str):
     return ec2_client
 
 
-def find_vpn_gateway_id(*, ec2_client, vpc_id: str):
+def is_vpc_available(*, ec2_client, vpc_id: str):
+    response = ec2_client.describe_vpcs(VpcIds=[vpc_id])
+
+    if response and isinstance(response, dict) and 'Vpcs' in response:
+        vpc = next(iter(response['Vpcs']), None)
+
+        if vpc:
+            state = vpc['State']
+
+            if state == 'available':
+                return True
+
+            logger.warning(f'found vpc {vpc_id} with state={state}')
+        else:
+            raise AwsException(f"Vpc not found {vpc_id}")
+
+    return False
+
+
+def find_attached_vpn_gateway(*, ec2_client, vpc_id: str):
+    response = ec2_client.describe_vpn_gateways()
+
+    if isinstance(response, dict) and 'VpnGateways' in response:
+        for gw in response['VpnGateways']:
+            attachments = gw['VpcAttachments']
+            attachment = next(filter(lambda a: a['VpcId'] == vpc_id and a['State'] == 'attached', attachments), None)
+
+            if attachment:
+                return gw['VpnGatewayId']
+
+    return None
+
+
+def find_vpn_gateway(*, ec2_client, vpn_id: str):
     response = ec2_client.describe_vpn_gateways()
 
     if response and isinstance(response, dict) and 'VpnGateways' in response:
         for gw in response['VpnGateways']:
-            attachments = gw['VpcAttachments']
-
-            for attachment in attachments:
-                if attachment['VpcId'] == vpc_id:
-                    return gw['VpnGatewayId']
+            if gw['VpnGatewayId'] == vpn_id:
+                return gw
 
     return None
+
+
+def attach_vpn_gateway(*, ec2_client, vpn_id: str, vpc_id: str):
+    vpn_gateway = find_vpn_gateway(ec2_client=ec2_client, vpn_id=vpn_id)
+    attachments = vpn_gateway['VpcAttachments']
+
+    for attachment in attachments:
+        if attachment['VpcId'] == vpc_id:
+            raise Exception(f"Already attached ...{attachment['State']}")
+
+    response = ec2_client.attach_vpn_gateway(
+        VpcId=vpc_id,
+        VpnGatewayId=vpn_id
+    )
+
+    attachment = response['VpcAttachment']
+    state = attachment['State']
+
+    if state == 'attached':
+        return
+
+    for i in range(RETRY):
+        vpn_gateway = find_vpn_gateway(ec2_client=ec2_client, vpn_id=vpn_id)
+        attachments = vpn_gateway['VpcAttachments']
+
+        for attachment in attachments:
+            if attachment['VpcId'] == vpc_id:
+                state = attachment['State']
+                break
+
+        if state == 'attached':
+            return
+
+        time.sleep(20)
+
+    raise AwsException(f"Timed out on attaching vpn_gateway: state={state}")
+
+
+def create_vpn_gateway(*, ec2_client, name: str, amazon_asn: int):
+    response = ec2_client.create_vpn_gateway(
+        Type='ipsec.1',
+        TagSpecifications=[
+            {
+                'ResourceType': 'vpn-gateway',
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': name
+                    },
+                ]
+            },
+        ],
+        AmazonSideAsn=amazon_asn
+    )
+
+    vpn_gateway = response['VpnGateway']
+    vpn_id = vpn_gateway['VpnGatewayId']
+    state = vpn_gateway['State']
+
+    if state == 'available':
+        return vpn_id
+
+    for i in range(RETRY):
+        vpn_gateway = find_vpn_gateway(ec2_client=ec2_client, vpn_id=vpn_id)
+        state = vpn_gateway['State']
+
+        if state == 'available':
+            return vpn_id
+
+        time.sleep(20)
+
+    raise AwsException(f"Timed out on creating vpn_gateway: state={state}")
 
 
 def create_direct_connect_client(*, region: str, access_key: str, secret_key: str):
@@ -56,39 +158,50 @@ def find_direct_connect_gateway_id(*, direct_connect_client, gateway_name: str):
     return None
 
 
-def create_direct_connect_gateway_id(*, direct_connect_client, gateway_name: str, vif_name: str):
+def create_direct_connect_gateway(*, direct_connect_client, gateway_name: str, amazon_asn: int):
     response = direct_connect_client.describe_direct_connect_gateways()
-
     direct_connect_gateway_id = None
 
     if response and isinstance(response, dict) and 'directConnectGateways' in response:
         for gw in response['directConnectGateways']:
             if gw['directConnectGatewayName'] == gateway_name:
+                logger.info(f"Found directConnectGateway {gw}")
                 direct_connect_gateway_id = gw['directConnectGatewayId']
                 break
 
     if not direct_connect_gateway_id:
         response = direct_connect_client.create_direct_connect_gateway(
             directConnectGatewayName=gateway_name,
-            amazonSideAsn=64512
+            amazonSideAsn=amazon_asn
         )
 
+        logger.info(f"Created directConnectGateway {response}")
         direct_connect_gateway_id = response['directConnectGateway']['directConnectGatewayId']
 
-    response = direct_connect_client.describe_virtual_interfaces()
-    found = False
+    return direct_connect_gateway_id
 
-    if response and isinstance(response, dict) and 'virtualInterfaces' in response:
+
+def create_private_virtual_interface(*, direct_connect_client, direct_connect_gateway_id: str, vif_name: str):
+    response = direct_connect_client.describe_virtual_interfaces()
+    details = {}
+
+    if isinstance(response, dict) and 'virtualInterfaces' in response:
         for vif in response['virtualInterfaces']:
             if vif['directConnectGatewayId'] == direct_connect_gateway_id and vif['virtualInterfaceName'] == vif_name:
-                found = True
+                logger.info(f"Found existing private virtual interface {vif_name}")
 
-    if not found:
-        direct_connect_client.create_private_virtual_interface(
-            connectionId='',
+                for k in VIF_DETAILS:
+                    details[k] = vif[k]
+
+                break
+
+    if not details:
+        logger.info(f"Creating private virtual interface {vif_name}")
+        vif = direct_connect_client.create_private_virtual_interface(
+            connectionId='dxcon-fgz7lrnh',
             newPrivateVirtualInterface={
                 'virtualInterfaceName': vif_name,
-                'vlan': 100,
+                'vlan': 2,
                 'asn': 53800,
                 'mtu': 9001,
                 'authKey': BGPKEY,
@@ -100,7 +213,21 @@ def create_direct_connect_gateway_id(*, direct_connect_client, gateway_name: str
             }
         )
 
-    return direct_connect_gateway_id
+        for k in VIF_DETAILS:
+            details[k] = vif[k]
+
+    for i in range(RETRY):
+        response = direct_connect_client.describe_virtual_interfaces()
+        vif = next(filter(lambda v: v[VIF_ID] == details[VIF_ID], response['virtualInterfaces']))
+        state = vif[VIF_STATE]
+
+        if state == 'down' or state == 'available':
+            logger.info(f"TODO: Breaking private virtual interface {vif_name}:state={state}")  # TODO fix the down state
+            break
+
+        time.sleep(20)
+
+    return details
 
 
 def find_association_dxgw_vpn(*, direct_connect_client, direct_connect_gateway_id: str, vpn_id: str):
