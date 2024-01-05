2216bcccbb7933a91a4ad3884a6c75f43802ff42
Abdelilah Essiari <aessiari@lbl.gov>
2024-01-05 09:32:56 -0800

    enhanced stats

--- fabfed/util/config.py
+++ fabfed/util/config.py
@@ -5,7 +5,12 @@ from typing import List
 
 class WorkflowConfig:
     def __init__(self, *, dir_path=None, content=None, var_dict=None):
+        import time
+
+        start = time.time()
         self.providers, self.resources = Parser.parse(dir_path=dir_path, content=content, var_dict=var_dict)
+        end = time.time()
+        self.parse_and_validate_config_duration = end - start
 
     def get_provider_config(self) -> List[ProviderConfig]:
         return self.providers
