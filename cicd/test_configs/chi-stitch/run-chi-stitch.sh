6c9dae281d24c9bde145cc4f291cb50783e566de
Abdelilah Essiari <aessiari@lbl.gov>
2024-01-20 01:40:08 -0800

    using cicd to test chameleon stitch

--- cicd/test_configs/chi-stitch/run-chi-stitch.sh
+++ cicd/test_configs/chi-stitch/run-chi-stitch.sh
@@ -1,30 +1,19 @@
 #!/usr/bin/env bash
 
 script_dir=$(dirname $0)
-echo $script_dir
 cd $script_dir
 
-cp controller.py /fabfed/controller/controller.py 
+set -ex
+cp ./controller.py /fabfed/controller/controller.py 
 pip install --break-system-package / > /dev/null 2>&1
 
 echo "PROJECT=$CHI_USER"
 sed "s/CHI_PROJECT/$CHI_PROJECT/" fabfed_credentials.yml.replaceme > ~/fabfed_credentials.yml
-cat ~/fabfed_credentials.yml
-
-string=$CHI_USER
-length=${#string}
-echo "LENGTH=$length"
-
-for ((i = 0; i < length; i++)); do
-    char="${string:i:1}"
-    echo "Character at position $i: $char"
-done
-
-exit 0
-
 sed -i "s/CHI_PASSWORD/$CHI_PASSWORD/" ~/fabfed_credentials.yml
 sed -i "s/CHI_USER/$CHI_USER/" ~/fabfed_credentials.yml
+cat ~/fabfed_credentials.yml
 
+set +ex
 # set -ex
 
 # fabfed workflow -s test-chi-stich -validate
