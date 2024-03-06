4b87a2b1fea4982d23f0f4356c662e979ab93ef3
Abdelilah Essiari <aessiari@lbl.gov>
2024-03-06 07:43:35 -0800

    ubuntu cicd

--- cicd/test_configs/fabric/run-fabfed.sh
+++ cicd/test_configs/fabric/run-fabfed.sh
@@ -9,6 +9,10 @@ mkdir -p ~/work/fabric_config
 mkdir -p ~/.fabfed
 
 ls -l /creds
+
 cp ../../fabfed_credentials.yml.cicd ~/.fabfed/
+sed -i "s/FABRIC_PROJECT/$FABRIC_PROJECT/" ~/.fabfed/fabfed_credentials.yml.cicd
+sed -i "s/FABRIC_USER/$FABRIC_USER/" ~/.fabfed/fabfed_credentials.yml.cicd
+
 fabfed workflow -s test-fabric -plan 
 exit $? 
