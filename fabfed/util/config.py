b57abc75f8a3446d080510cdcb73095c1537d6c3
Abdelilah Essiari <aessiari@lbl.gov>
2024-03-13 00:47:19 -0700

    refactoring high level fabfed api

--- fabfed/util/config.py
+++ fabfed/util/config.py
@@ -1,19 +1,21 @@
 from .parser import Parser
 from .config_models import ResourceConfig, ProviderConfig
-from typing import List
+from typing import List, Union, Dict
 
 
 class WorkflowConfig:
-    def __init__(self, *, dir_path=None, content=None, var_dict=None):
-        import time
+    def __init__(self, *, provider_configs: List[ProviderConfig], resource_configs: List[ResourceConfig]):
+        self.provider_configs = provider_configs
+        self.resource_configs = resource_configs
 
-        start = time.time()
-        self.providers, self.resources = Parser.parse(dir_path=dir_path, content=content, var_dict=var_dict)
-        end = time.time()
-        self.parse_and_validate_config_duration = end - start
-
-    def get_provider_config(self) -> List[ProviderConfig]:
-        return self.providers
+    def get_provider_configs(self) -> List[ProviderConfig]:
+        return self.provider_configs
 
     def get_resource_configs(self) -> List[ResourceConfig]:
-        return self.resources
+        return self.resource_configs
+
+    @staticmethod
+    def parse(*, dir_path: Union[str, None] = None, content: Union[str, None] = None,
+              var_dict: Union[Dict, None] = None):
+        provider_configs, resource_configs = Parser.parse(dir_path=dir_path, content=content, var_dict=var_dict)
+        return WorkflowConfig(provider_configs=provider_configs, resource_configs=resource_configs)
